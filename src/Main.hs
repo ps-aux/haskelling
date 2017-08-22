9ceb9d9c776ebf3c5863413acf0310d1e9840a54
Lukas <seriouz.accz@gmail.com>
2017-08-23 00:01:42 +0200

    Add drawing of some shapes

--- src/Main.hs
+++ src/Main.hs
@@ -2,39 +2,55 @@ module Main where
 
 main :: IO ()
 main = do
-    let x = 5
+    let x = 20
     let separator = "\n"
-    putStrLn $ center "xxxx" 2
---     putStrLn $ rightTriangle x
---     putStrLn separator
---     putStrLn $ equilateralTriangle x
+    putStrLn $ leftRightTriangle x
+    putStrLn $ rightRightTriangle x
+    putStrLn $ equilateralTriangle x
+    putStrLn $ square x
 
-iArr = [1,2,3]
-chArr = ['a','b','c', 'y']
-strAarr = ["a", "bb", "ccc"]
-
-rightTriangle :: Int -> String
-rightTriangle n = loop n 1 ""
-    where loop height lvl acc
-            | lvl > height = acc
-            | otherwise    = loop height (lvl + 1) (acc  ++ drawLine lvl ++ "\n")
+shape :: (Int -> Int -> String) -> Int -> String
+shape lvlToString height = loop height 1 ""
+    where loop height lvl acc =
+            if lvl > height
+                then acc
+                else loop height (lvl + 1) (acc  ++ lvlToString height lvl ++ "\n")
 
 
-equilateralTriangle :: Int -> String
-equilateralTriangle n = loop n 1 ""
-    where loop height lvl acc
---             | lvl > height = acc
-            | otherwise    = loop height (lvl + 2) (acc ++ "\n" ++ drawLine lvl)
+leftRightTriangle = shape $ \_ l -> line l
+
+rightRightTriangle = shape $ \h l -> spaceLeftPad (line l) h
+
+square = shape  $ \h _ -> line h
+
+equilateralTriangle = shape equilateralTriangleLvl
+equilateralTriangleLvl height lvl = spaceCenter (line (lvlWidth lvl)) baseWidth
+    where baseWidth =  lvlWidth height
+          lvlWidth l = l * 2 - 1
 
 
-nString :: Char -> Int -> String
-nString ch n = take n (iterate id ch)
+line width = replicate width '*'
 
-drawLine = nString 'x'
+center :: Char -> String -> Int -> String
+center fillCh str width =
+    if len > width
+        then error "String longer than width"
+        else fill leftPad ++ str ++ fill rightPad
+    where len = length str
+          fill n = replicate n fillCh
+          leftPad = quot (width - len) 2
+          rightPad = width - len - leftPad
 
-center :: String -> Int -> String
-center str width
-    | length str > width = error "String longer than width"
+leftPad :: Char -> String -> Int -> String
+leftPad fillCh str width =
+    if len > width
+        then error "String longer than width"
+        else replicate leftPad fillCh ++ str
+    where len = length str
+          leftPad = width - len
+
+spaceCenter = center ' '
+spaceLeftPad = leftPad ' '
 
 join :: String -> [String] -> String
 join _ (x:[]) = x
