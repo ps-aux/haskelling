3d6353273c0c24590d1f6fd74ef360fb106cf8d1
Lukas <seriouz.accz@gmail.com>
2017-08-22 22:42:01 +0200

    Some more stuff

--- src/Main.hs
+++ src/Main.hs
@@ -2,28 +2,39 @@ module Main where
 
 main :: IO ()
 main = do
-    putStrLn $ triangle 7
+    let x = 5
+    let separator = "\n"
+    putStrLn $ center "xxxx" 2
+--     putStrLn $ rightTriangle x
+--     putStrLn separator
+--     putStrLn $ equilateralTriangle x
 
 iArr = [1,2,3]
 chArr = ['a','b','c', 'y']
 strAarr = ["a", "bb", "ccc"]
 
-px = "x"
-triangle :: Int -> String
-triangle n = loop n 1 ""
-    where
-          draw = nString 'x'
-          loop height lvl acc
+rightTriangle :: Int -> String
+rightTriangle n = loop n 1 ""
+    where loop height lvl acc
             | lvl > height = acc
-            | otherwise     = loop height (lvl + 1) (acc ++ "\n" ++ draw lvl)
+            | otherwise    = loop height (lvl + 1) (acc  ++ drawLine lvl ++ "\n")
 
-calcLines :: Int -> [Int]
-calcLines 1 = [1]
-calcLines x =  (calcLines(x - 1)) ++ [x * 2 - 1]
+
+equilateralTriangle :: Int -> String
+equilateralTriangle n = loop n 1 ""
+    where loop height lvl acc
+--             | lvl > height = acc
+            | otherwise    = loop height (lvl + 2) (acc ++ "\n" ++ drawLine lvl)
 
 
 nString :: Char -> Int -> String
 nString ch n = take n (iterate id ch)
+
+drawLine = nString 'x'
+
+center :: String -> Int -> String
+center str width
+    | length str > width = error "String longer than width"
 
 join :: String -> [String] -> String
 join _ (x:[]) = x
